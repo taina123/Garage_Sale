c7916f160daca92c9d0133d47a140a6b70318801
taina123 <taina.medeiros97@yahoo.com.br>
2016-11-04 21:47:01 -0200

    GarageSale

---
+++ GarageSale/GarageSale_v0/script.js
@@ -0,0 +1,15 @@
+function relayImg(){
+	var i=1;
+	
+	if (i == 1){
+		document.getElementById("image1").src="image1.png";
+    } 
+	
+	else if (i == 2) {       
+    	document.getElementById("image2").src="image2.png";
+    }
+	
+	else if (i == 3){
+		document.getElementById("image3").src="image3.png";
+	}
+}
